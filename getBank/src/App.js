5f51e3941a3b8f9d87cd9455c0b256d4d2f07f6f
praveen-me <praveenkrsaini0@gmail.com>
2019-01-17 23:05:17 +0530

    Initialize socket

--- getBank/src/App.js
+++ getBank/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import io from 'socket.io-client';
 
 import './scss/app.scss';
+
+const socket = io('http://localhost:8001')
 
 class App extends Component {
   render() {
