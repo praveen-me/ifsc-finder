7da69fd77ad13f8b3b18ac6992de0091dac8da40
praveen-me <praveenkrsaini0@gmail.com>
2019-01-19 13:33:47 +0530

    Add ifsc-cli core

--- ifsc-checker-CLI/cli.js
+++ ifsc-checker-CLI/cli.js
@@ -1,6 +1,11 @@
 const https = require('https');
 const readLine = require('readline');
 const chalk = require('chalk');
+const logUpdate = require('log-update');
+const spinner = require('elegant-spinner');
+
+let isLoading = false;
+const frame = spinner();
 
 const line = readLine.createInterface({
   input: process.stdin,
@@ -9,13 +14,13 @@ const line = readLine.createInterface({
 
 const convertReadable = (str) => {
   return str ? str.split(' ')
-        .filter(str => str.length > 0)
-        .map(str => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`)
-        .join(' ') : 'Not available';
+  .filter(str => str.length > 0)
+  .map(str => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`)
+  .join(' ') : 'Not available';
 }
 
 const blueText = (head, text)  => {
-  return console.log(`${chalk.rgb(0, 188, 212).bold(`${head} - ${convertReadable(text)}`)}`);
+  return console.log(`${chalk.rgb(0, 188, 212)(`${chalk.magenta.bold(head)} ${chalk.white('-')} ${convertReadable(text)}`)}`);
 }
 
 const displayBankDetail = (bank) => {
@@ -32,12 +37,36 @@ const displayBankDetail = (bank) => {
 
 // Ifsc KKBK0000261
 
+const errMsg = (msg) => console.log(`\n${chalk.red.bold(msg)}\n`);
+
 line.question(chalk.yellow('\nEnter IFSC Code => '), (ifsc) => {
   // fetching data
   https.get(`https://ifsc.razorpay.com/${ifsc}`, (data) => {
+    isLoading = true;
+    let interval;
+    // Set loading on isLoading
+    if(isLoading) {
+      interval =  setInterval(function(){
+        logUpdate(`
+          ${ `${chalk.rgb(141, 249, 255).bold(`Waiting for data ${frame()}`)}`}
+        `);
+      }, 50);
+    }
+    
     data.on('data', (d) => {
-      displayBankDetail(JSON.parse(String(d)));
-    });
+      const bankData = JSON.parse(String(d))
+
+      // isLoading = false;
+      // Clearing the interval of spinner
+      if (!isLoading) {
+        clearInterval(interval);
+        if (typeof(bankData) === 'object') {
+          displayBankDetail(bankData);
+        } else {
+          errMsg('IFSC CODE IS WORNG. Please Try Again.')
+        }
+      }
+    })
   });
   line.close();
 });
