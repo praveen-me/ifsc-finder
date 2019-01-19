0396a9abf0e5248b139a91901dfd5affed2543d5
praveen-me <praveenkrsaini0@gmail.com>
2019-01-19 15:31:22 +0530

    Add eslint changes in cli

--- ifsc-checker-CLI/cli.js
+++ ifsc-checker-CLI/cli.js
@@ -16,17 +16,15 @@ const line = readLine.createInterface({
 // convert totally capitalized string into readable
 const convertReadable = (str) => {
   return str ? str.split(' ')
-  .filter(str => str.length > 0)
-  .map(str => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`)
-  .join(' ') : 'Not available';
-}
+    .filter(s => s.length > 0)
+    .map(s => `${s[0].toUpperCase()}${s.slice(1).toLowerCase()}`)
+    .join(' ') : 'Not available';
+};
 
-const blueText = (head, text) => {
-  return console.log(`${chalk.rgb(0, 188, 212)(`${chalk.magenta.bold(head)} ${chalk.white('-')} ${convertReadable(text)}`)}`);
-}
+const blueText = (head, text) => console.log(`${chalk.rgb(0, 188, 212)(`${chalk.magenta.bold(head)} ${chalk.white('-')} ${convertReadable(text)}`)}`);
 
 const displayBankDetail = (bank) => {
-  const { BRANCH, ADDRESS, CONTACT, DISTRICT, STATE, BANK, BANKCODE, IFSC } = bank;
+  const { BRANCH, ADDRESS, CONTACT, DISTRICT, STATE, BANK, IFSC } = bank;
   console.log(`\n${chalk.rgb(141, 249, 255).bold('BANK DETAILS')}\n`);
   blueText('Bank Name', BANK);
   blueText('IFSC Code', IFSC);
@@ -37,44 +35,40 @@ const displayBankDetail = (bank) => {
   blueText('Bank State', STATE);
 };
 
-
-// Ifsc KKBK0000261
-
-const errMsg = (msg) => console.log(`\n${chalk.red.bold(msg)}\n`);
+const errMsg = (msg) => {
+  console.log(`\n${chalk.red.bold(msg)}\n`);
+};
 
 const endMessage = () => console.log(`\n${chalk.rgb(141, 249, 255).bold('Thanks for using IFSC Checker.')}\n`);
 
 line.question(chalk.yellow('\nEnter IFSC Code => '), (ifsc) => {
-  
   // fetching data
   isLoading = true;
-  
-  // Set loading on KKBK0000261isLoading
+  // Set loader
   if (isLoading) {
-    interval =  setInterval(function(){
+    interval = setInterval(() => {
       logUpdate(`
       ${ `${chalk.rgb(141, 249, 255).bold(`Waiting for data ${frame()}`)}`}
       `);
     }, 50);
   }
-  
+  // getting data from the endpoint
   https.get(`https://ifsc.razorpay.com/${ifsc}`, (data) => {
     data.on('data', (d) => {
       isLoading = false;
-      const bankData = JSON.parse(String(d))
-      
+      const bankData = JSON.parse(String(d));
       // Clearing the interval of spinner
       if (!isLoading) {
         clearInterval(interval);
-        if (typeof(bankData) === 'object') {
+        if (typeof (bankData) === 'object') {
           displayBankDetail(bankData);
           endMessage();
         } else {
-          errMsg('IFSC CODE IS WORNG. Please Try Again.')
+          errMsg('IFSC CODE IS WRONG. Please Try Again.');
           endMessage();
         }
       }
-    })
+    });
   });
   line.close();
 });
